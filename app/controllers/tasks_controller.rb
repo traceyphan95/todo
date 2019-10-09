4fcf1f23582cdb7a5ff1461a5cf0220d323f59c4
Tracey <traceyphan95@gmail.com>
2019-10-09 13:57:42 +0000

    Task Creation API

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,16 +3,20 @@ class TasksController < ApplicationController
     render json: Task.order(:id)
   end
 
-
   def update
     task = Task.find(params[:id])
     task.update_attributes(task_params)
     render json: task
   end
 
-private
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
+  private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,17 +5,13 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
-      task1.update_attributes(title: "Something else")
       get :index
-      expect(response).to have_http_status :success
+      assert_response :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      response_ids = response_value.collect do |task|
-        task["id"]
-      end
-      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
+
   describe "tasks#update" do
     it "should allow tasks to be marked as done" do
       task = FactoryBot.create(:task, done: false)
@@ -26,4 +22,13 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end
+  end
 end
